2380825626fd69c0efd8d14931aff28451a336f5
Gabriel André Melo <44413822+gabrielMelo42@users.noreply.github.com>
2020-05-27 21:16:40 -0300

    Add the class Node and create stack top
    Class Node represents one node of the stack

---
+++ Stacks/Dynamic Stack/DynamicStack.java
@@ -0,0 +1,15 @@
+class Node{
+    private int data;
+    private Node next;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+public class DynamicStack{
+    private Node top;
+    public static void main(String [] args){
+        
+    }
+}
