92f44c9a6a33257fb6930c69bdd23020a122a6f7
Gabriel André Melo <44413822+gabrielMelo42@users.noreply.github.com>
2020-05-29 11:21:20 -0300

    Modified the atributes of the class

--- Stacks/Dynamic Stack/DynamicStack.java
+++ Stacks/Dynamic Stack/DynamicStack.java
@@ -1,8 +1,4 @@
 public class DynamicStack{
-    public static void main(String [] args){
-        Node top = new Node();
-        top.data = 2;
-        System.out.println(top.data); 
-    }
-    
+    Node top;
+    public static void main(String [] args){}
 }
