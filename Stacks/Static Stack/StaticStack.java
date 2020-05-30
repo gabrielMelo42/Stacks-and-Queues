4f54792995bbebf80ce20399ca5ee157c6cd5198
Gabriel André Melo <44413822+gabrielMelo42@users.noreply.github.com>
2020-05-29 22:11:58 -0300

    Added initialize method

--- Stacks/Static Stack/StaticStack.java
+++ Stacks/Static Stack/StaticStack.java
@@ -1,6 +1,13 @@
 public class StaticStack{
     int [] stack;
-    int top; 
+    int top;
+    
+    void initialize(){
+        for(int i = 0; i < stack.length; i++){
+            stack[i].data = -1;
+        }
+        top = -1;
+    }
     
     public static void main(String [] args){}
 }
