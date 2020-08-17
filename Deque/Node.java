c39f05ca41535a7e79f97a8ebcefcfcea975d518
Gabriel André Melo <44413822+gabrielMelo42@users.noreply.github.com>
2020-08-17 11:41:09 -0300

    Create Node of Deque class

---
+++ Deque/Node.java
@@ -0,0 +1,11 @@
+public class Node{
+    private int data;
+    private Node next;
+    private Node previous;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+}
